a3d6a8edd0043227361774727dd8fa716bac6e91
axcwg <xiey0@qq.com>
2024-10-02 18:31:03 +0800

    For the sake of syncing and init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+function navbar() {
+  return (
+    <>
+      <div className='navbar'>
+        <div>
+          <a href='https://axcwg.github.io'>Back</a>
+          
+        </div>
+        <div>
+        <a href=''>Latest</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {navbar()}
+      <div>
+        <div className='picture'>
+          <img src='/assets/img/test 拷贝.png'></img>
+        </div>
+      </div>
+    </>
+
   );
 }
 
